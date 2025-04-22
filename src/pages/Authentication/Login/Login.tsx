0f7ade1e3989999ceda20ce89a7bc63747bfba5d
firdavs9777 <fmutalipov97@gmail.com>
2025-04-22 18:03:37 +0900

    new changes

--- src/pages/Authentication/Login/Login.tsx
+++ src/pages/Authentication/Login/Login.tsx
@@ -23,7 +23,7 @@ const Login = () => {
   const redirect = sp.get("redirect") || "/";
 
   useEffect(() => {
-    if (userInfo) {
+    if (userInfo?.token) {
       navigate(redirect);
     }
   }, [userInfo, redirect, navigate]);
